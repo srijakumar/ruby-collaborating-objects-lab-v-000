6416335372bedfdf21c6e3101efdee40499cdbe5
Srija S <srijakumar7@gmail.com>
2018-05-05 19:49:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,6 +8,14 @@ end
 
 def self.new_by_filename (filename)
   song_and_artist = filename.split("-")
+  new_song= self.new(song)
+  new_song.artist_name = artist
+end
+
+def artist_name
+  self.artist = Artist.find_or_create_by_name(name)
+  artist.add_song(self)
+end
 end
 
 
